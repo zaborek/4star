6f469927e2a45c016c026904bcda9e0f4d384fa2
zaborek <zaborek@biostat.wisc.edu>
2019-10-30 10:34:45 -0500

    removed records

--- cards.r
+++ cards.r
@@ -9,11 +9,30 @@ director = dat$X.2
 runtime = dat$X
 dvd.date = dat$X.12
 genre = dat$SEE
+cc = dat$N
+cc1 = dat$N.1
 
 
-dat = list(title, genre, format, barcode, director, runtime, dvd.date)
+dat = list(title, genre, format, barcode, director, runtime, dvd.date, cc, cc1)
 dat=lapply(dat, as.character)
 
 dat = data.frame(matrix(unlist(dat), nrow=length(dat[[1]]), byrow=F))
-colnames(dat) = c("title", "genre","format", "barcode", "director", "runtime", "dvd_date")
-dat[988:1100,]
+colnames(dat) = c("title", "genre","format", "barcode", "director",
+                  "runtime", "dvd_date", "cc", "cc1")
+
+
+# remove records:
+#sold:
+idx.sold = which(dat$genre %in% c("Z", "ZDA", "ZDC", "ZDD", "ZDE", "ZDF", "ZDR", "ZDS", "ZDV"))
+dat = dat[-idx.sold,]
+
+#goto:
+idx.goto = which(dat$genre == "SEE")
+dat = dat[-idx.goto,]
+
+#pornos:
+idx.porno = which(dat$genre == "X")
+dat = dat[-idx.porno,]
+
+
+
