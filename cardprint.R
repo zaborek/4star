64897b0850df6bfbdd1c123d70a73ad28d2bfb11
zaborek <zaborek@biostat.wisc.edu>
2019-11-01 13:11:10 -0500

    big yoshee

--- cardprint.R
+++ cardprint.R
@@ -6,58 +6,83 @@ library(grid)
 infile = paste0(getwd(),"/cards.rds")
 dat=readRDS(infile)
 
-dat = dat[81:90,]
 
-pdf("out.pdf", height=8.5, width=11)
-par(mfcol=c(2,2)) # fills in by column
-par(mar=c(2,1,2,1)) # bottom, left, top, right
 
-for(i in 1:nrow(dat)){
-  datt = dat[i,]
+
+
+printcardpdf = function(dat, outfile){
+  pdf(outfile, height=8.5, width=11)
+  par(mfcol=c(2,2)) # fills in by column
+  par(mar=c(2,1,2,1)) # bottom, left, top, right
   
-  
-  plot.new()
-  polygon(c(0,0,1,1), c(0,1,1,0), border="black", lwd=3)
-  text(.02, .95, "Documentary", pos=4)
-  text(.5,.8,datt$title, cex=1.2)
-  text(.5,.6, paste0("Director: ", datt$director))
-  text(.5,.5, paste0("Runtime: " , datt$runtime, " Minutes"))
-  text(.5,.4, paste0("Closed Caption: ", datt$cc))
-  text(.02, .05, datt$format, pos=4)
-  
-  # put in barcode
-  if(nchar(datt$barcode)==8){
-    pngfile = paste0('barcode_png/', datt$barcode, '.png')
-    if(file.exists(pngfile)){
-      mypng = readPNG(pngfile)
-      
-      # top left
-      if(i%%4 ==1){
-      grid.raster(mypng, .25, .62, width=.1)
-      }
-      # bottom left
-      else if (i%%4 == 2){
-        grid.raster(mypng, .25, .12, width=.1)
-      }
-      # top right
-      else if (i%%4 == 3){
-        grid.raster(mypng, .75, .62, width=.1)
-      }
-      # bottom right
-      else if (i%%4 == 0){
-        grid.raster(mypng, .75, .12, width=.1)
+  for(i in 1:nrow(dat)){
+    datt = dat[i,]
+    
+    
+    plot.new()
+    polygon(c(0,0,1,1), c(0,1,1,0), border="black", lwd=3)
+    top.text = datt$genre
+    if(nchar(datt$country)>0){
+      top.text = paste0(top.text, " - ", datt$country)
+    }
+    if(datt$subtitles=="Subtitled"){
+      top.text = paste0(top.text, " - ", datt$subtitles)
+    }
+    text(.02, .95, top.text, pos=4)
+    text(.5,.77,datt$title, cex=1.28)
+    text(.5,.6, paste0("Director: ", datt$director))
+    text(.5,.52, paste0("Play Time: " , datt$runtime, " Minutes"))
+    text(.5,.46, paste0("Year: " , datt$rel_year))
+    text(.5,.38, paste0("Closed Captioned: ", datt$cc))
+    text(.02, .05, datt$format, pos=4)
+    
+    # put in barcode
+    if(nchar(datt$barcode)==8){
+      pngfile = paste0('barcode_png/', datt$barcode, '.png')
+      if(file.exists(pngfile)){
+        mypng = readPNG(pngfile)
+        
+        # top left
+        png.w = .17
+        if(i%%4 ==1){
+          grid.raster(mypng, .25, .62, width=png.w)
+        }
+        # bottom left
+        else if (i%%4 == 2){
+          grid.raster(mypng, .25, .12, width=png.w)
+        }
+        # top right
+        else if (i%%4 == 3){
+          grid.raster(mypng, .75, .62, width=png.w)
+        }
+        # bottom right
+        else if (i%%4 == 0){
+          grid.raster(mypng, .75, .12, width=png.w)
+        }
       }
     }
+    polygon(c(0,0,1,1), c(0,1,1,0), border="black", lwd=3)
+    
+    
+    
+    # if a multiple of 4, create new page in pdf
+    if(i%%4 == 0){
+      #grid.newpage()
+    }
   }
+  dev.off()
   
-  
-  
-  # if a multiple of 4, create new page in pdf
-  if(i%%4 == 0){
-    #grid.newpage()
-  }
 }
 
+genres = sort(unique(dat$genre))
 
-
-dev.off()
+for(genre in genres){
+  cat("printing ", genre)
+  cat("\n")
+  outdvd = paste0("/outpdf/",tolower(genre),"_dvd.pdf")
+  outbd = paste0("/outpdf/",tolower(genre),"_bd.pdf")
+  
+  printcardpdf(dat[dat$genre==genre&dat$format=="DVD",], outdvd)
+  printcardpdf(dat[dat$genre==genre&dat$format=="Blu-ray",], outbd)
+}
+cat("done")
--- cards.r
+++ cards.r
@@ -7,18 +7,46 @@ format = dat$DVD
 barcode = dat$X.8
 director = dat$X.2
 runtime = dat$X
+rel.year = dat$X00 + 2000
 dvd.date = dat$X.12
 genre = dat$SEE
 cc = dat$N
 cc1 = dat$N.1
+subbed = as.character(dat$X.1)
+
+# fix release years ##################################
+dvd.date = as.character(dvd.date)
+dvd.date = as.Date(dvd.date, "%m/%d/%y")
+dvd.year = as.numeric(substr(dvd.date,1,4))
+
+idx.oops = which(dvd.year < rel.year)
+rel.year[idx.oops] = rel.year[idx.oops]-100
 
 
-dat = list(title, genre, format, barcode, director, runtime, dvd.date, cc, cc1)
+# fix sub and country information #####################
+idx.sub = grep(pattern="\\*S", subbed)
+
+country.info = trimws(gsub("(\\.|\\*|\\d).*", "", subbed[idx.sub]))
+country.info = gsub(",\\s", "/", country.info)
+country.info = gsub("/\\s", "/", country.info)
+country.info[which(nchar(country.info)<2)] = ""
+
+sub.info = country = rep("", nrow(dat))
+sub.info[idx.sub] = "Subtitled"
+country[idx.sub] = country.info
+
+
+
+
+
+
+
+dat = list(title, genre, format, barcode, director, runtime, rel.year, dvd.year, dvd.date, cc, cc1, country, sub.info)
 dat=lapply(dat, as.character)
 
 dat = data.frame(matrix(unlist(dat), nrow=length(dat[[1]]), byrow=F), stringsAsFactors = F)
 colnames(dat) = c("title", "genre","format", "barcode", "director",
-                  "runtime", "dvd_date", "cc", "cc1")
+                  "runtime", "rel_year", "dvd_year", "dvd_date", "cc", "cc1", "country", "subtitles")
 
 
 # remove records:
@@ -34,9 +62,40 @@ dat = dat[-idx.goto,]
 idx.porno = which(dat$genre == "X")
 dat = dat[-idx.porno,]
 
+#cleaners:
+idx.cln = which(dat$genre == "CLN")
+dat = dat[-idx.cln,]
+
 
 # add two leading zeros barcode scanning
 dat$barcode = paste0("00", dat$barcode)
+
+
+# Edit genre info to be full
+dat$genre[dat$genre=="A"] = "Action"
+dat$genre[dat$genre=="C"] = "Comedy"
+dat$genre[dat$genre=="D"] = "Drama"
+dat$genre[dat$genre=="DR6"] = "Drama"
+dat$genre[dat$genre=="E"] = "Documentary"
+dat$genre[dat$genre=="EFR"] = "Documentary"
+dat$genre[dat$genre=="F"] = "Foreign"
+dat$genre[dat$genre=="H"] = "Horror"
+dat$genre[dat$genre=="J"] = "Juvenile"
+dat$genre[dat$genre=="M"] = "Music"
+dat$genre[dat$genre=="Q"] = "Silent"
+dat$genre[dat$genre=="R"] = "Television"
+dat$genre[dat$genre=="S"] = "Science Fiction"
+dat$genre[dat$genre=="T"] = "Anime"
+dat$genre[dat$genre=="V"] = "Mystery"
+dat$genre[dat$genre=="W"] = "Western"
+
+#edit format to be blu ray not bd
+dat$format[dat$format=="BD"] = "Blu-ray"
+
+#edit Y to Yes and N to NO
+dat$cc[dat$cc=="N"] = "No"
+dat$cc[dat$cc=="Y"] = "Yes"
+
 
 
 outfile = paste0(getwd(),"/cards.rds")
